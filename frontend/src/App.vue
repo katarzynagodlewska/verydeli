628f2f8c3bc1feb8fa6834c4268475d876c6b8a7
Kasia <katarzyna-godlewska1@wp.pl>
2020-08-17 14:27:18 +0200

    Added vue.js and Footer component

---
+++ frontend/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <Navbar />
+    <router-view></router-view>
+    <Footer />
+  </div>
+</template>
+
+<script lang="ts">
+import Footer from "./components/Footer.vue";
+
+export default {
+  name: "App",
+  components: { Footer },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ frontend/src/components/Footer.vue
@@ -0,0 +1,48 @@
+<template>
+  <footer>
+    <h2>verydeli</h2>
+    <div>
+      <img src="../assets/img/ios.svg" />
+      <img src="../assets/img/android.png" />
+    </div>
+    <div>
+      <h4>Join to verydeli</h4>
+      <ul>
+        <li>
+          <a>User account</a>
+        </li>
+        <li>
+          <a>Restaurant account</a>
+        </li>
+        <li>
+          <a>Courier account</a>
+        </li>
+      </ul>
+    </div>
+    <div>
+      <p>Created by Katarzyna Godlewska</p>
+      <ul class="social">
+        <li class="social__item">
+          <a class="social__link" href="https://www.facebook.com/katarzyna.godlewska.71">
+            <img class="social__icon" src="../assets/img/icon-fb.svg" alt="facebook" />
+          </a>
+        </li>
+        <li class="social__item">
+          <a class="social__link" href="https://github.com/katarzynagodlewska">
+            <img class="social__icon" src="../assets/img/icon-github.svg" alt="github" />
+          </a>
+        </li>
+        <li class="social__item">
+          <a class="social__link" href="https://www.instagram.com/world_around_kate/">
+            <img class="social__icon" src="../assets/img/icon-instagram.svg" alt="instagram" />
+          </a>
+        </li>
+      </ul>
+    </div>
+  </footer>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({});
+</script>
