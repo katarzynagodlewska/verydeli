7c4c2c77ff8611e7f1c7290fda81c371f84e8c60
Kasia <katarzyna-godlewska1@wp.pl>
2020-08-17 22:50:09 +0200

    Changed project to TypeScript and few improvements

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,25 +1,26 @@
 <template>
   <div id="app">
     <Navbar />
-    <router-view>
-      <div>
-        <span class="text-repetateble-vertically">verydeli</span>
-      </div>
-    </router-view>
+    <div>
+      <span class="text-repetateble-vertically">verydeli</span>
+    </div>
     <Footer />
   </div>
 </template>
 
 <script lang="ts">
+import Vue from "vue";
 import Footer from "./components/Footer.vue";
 import Navbar from "./components/Navbar.vue";
-
-export default {
+export default Vue.extend({
   name: "App",
-  components: { Footer, Navbar },
-};
+  components: {
+    Footer,
+    Navbar,
+  },
+});
 </script>
 
-<style lang="scss">
-@import "../styles/scss/main.scss";
+<style lang="scss" scoped>
+@import "styles/main.scss";
 </style>
