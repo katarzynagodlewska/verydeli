b43655f462451e93e4fb407e48ca286b8998ca06
Kasia <katarzyna-godlewska1@wp.pl>
2020-08-17 14:58:25 +0200

    Created Navbar component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -8,20 +8,15 @@
 
 <script lang="ts">
 import Footer from "./components/Footer.vue";
+import Navbar from "./components/Navbar.vue";
 
 export default {
   name: "App",
-  components: { Footer },
+  components: { Footer, Navbar },
 };
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,10 @@
+<template>
+  <nav>
+    <img src="../assets/img/icon-log.svg" />
+  </nav>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({});
+</script>
