fca0e15f1eee2d32052d12685fa4ca0b06e56718
Kasper Cisewski <kaspercisewski@gmail.com>
2020-08-17 23:26:13 +0200

    https://trello.com/c/BliM0xVE || initialized solution projects

---
+++ backend/VeryDeli.Data/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VeryDeli.Data
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
