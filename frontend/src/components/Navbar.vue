7d245f55f7fde577cebd55308091212d06080f50
Kasia <katarzyna-godlewska1@wp.pl>
2020-08-18 12:24:15 +0200

    https://trello.com/c/sxB7gx0a || Added box to log and register to icon on homepage

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -1,6 +1,10 @@
 <template>
   <nav>
-    <img src="../assets/img/icon-log.svg" />
+    <img class="icon-log" src="../assets/img/icon-log.svg" />
+    <div class="box-to-log-and-register">
+      <a href="" class="link-log">Log in</a>
+      <a href="" class="link-register">Sign up</a>
+    </div>
   </nav>
 </template>
 
@@ -12,3 +16,7 @@ import { Component, Vue } from "vue-property-decorator";
 })
 export default class Navbar extends Vue {}
 </script>
+
+<style lang="scss" scoped>
+@import "../styles/navbar.scss";
+</style>
