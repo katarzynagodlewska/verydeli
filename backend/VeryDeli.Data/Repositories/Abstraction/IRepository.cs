c6a05206f0126c9c4f66d05e9687a4931a9e9b42
Kasper Cisewski <kaspercisewski@gmail.com>
2020-08-21 00:26:30 +0200

    https://trello.com/c/BliM0xVE || added crud repository and repository interface marker

---
+++ backend/VeryDeli.Data/Repositories/Abstraction/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+
+namespace VeryDeli.Data.Repositories.Abstraction
+{
+    public interface IRepository<T> where T : class
+    {
+        IQueryable<T> GetAll();
+        Task Add(T entity);
+        Task Update(T entity);
+        Task Remove(T entity);
+        Task RemoveById(object id);
+        Task<T> GetById(object id);
+    }
+}
---
+++ backend/VeryDeli.Data/Repositories/CrudRepository.cs
@@ -0,0 +1,51 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using VeryDeli.Data.Repositories.Abstraction;
+
+namespace VeryDeli.Data.Repositories
+{
+    public class CrudRepository<T> : IRepository<T> where T : class
+    {
+        protected readonly VeryDeliDataContext Context;
+        private readonly DbSet<T> _table = null;
+
+        public CrudRepository(VeryDeliDataContext context)
+        {
+            Context = context;
+            _table = Context.Set<T>();
+        }
+
+        public async Task Add(T entity)
+        {
+            await _table.AddAsync(entity);
+            await Context.SaveChangesAsync();
+        }
+
+        public IQueryable<T> GetAll()
+        {
+            return _table.AsQueryable();
+        }
+        public async Task<T> GetById(object id)
+        {
+            return await _table.FindAsync(id);
+        }
+
+        public async Task Remove(T entity)
+        {
+            _table.Remove(entity);
+            await Context.SaveChangesAsync();
+        }
+        public async Task RemoveById(object id)
+        {
+            var entity = await GetById(id);
+            await Remove(entity);
+        }
+
+        public async Task Update(T entity)
+        {
+            _table.Update(entity);
+            await Context.SaveChangesAsync();
+        }
+    }
+}
