b8206e4f0da9f85d8ba0d98fc02b5e03a6267ed8
Kasper Cisewski <kaspercisewski@gmail.com>
2020-08-21 12:08:04 +0200

    https://trello.com/c/BliM0xVE || added user repository

---
+++ backend/VeryDeli.Data/Repositories/Implementation/UserRepository.cs
@@ -0,0 +1,12 @@
+﻿using VeryDeli.Data.Domains;
+using VeryDeli.Data.Repositories.Abstraction;
+
+namespace VeryDeli.Data.Repositories.Implementation
+{
+    public class UserRepository : CrudRepository<User>, IUserRepository
+    {
+        public UserRepository(VeryDeliDataContext context) : base(context)
+        {
+        }
+    }
+}
