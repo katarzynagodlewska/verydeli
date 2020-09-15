8b9d4c1cb8db1c788ed62bd61267e6ee0b256edd
Kasper Cisewski <kaspercisewski@gmail.com>
2020-09-15 23:44:58 +0200

    https://trello.com/c/oB9NShqU || added few first changes in integration test

---
+++ backend/Tests/VeryDeli.Tests.Integration/Controllers/Food/SearchTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Hosting;
+using VeryDeli.Api;
+using Xunit;
+
+namespace VeryDeli.Tests.Integration.Controllers.Food
+{
+    public class SearchTests
+    {
+        public SearchTests()
+        {
+
+        }
+
+        [Fact]
+        public async Task BasicEndPointTest()
+        {
+            // Arrange
+            var hostBuilder = new HostBuilder()
+                .ConfigureWebHost(webHost =>
+                {
+                    // Add TestServer
+                    webHost.UseTestServer();
+                    webHost.UseStartup<Startup>();
+                });
+
+            // Create and start up the host
+            var host = await hostBuilder.StartAsync();
+
+            // Create an HttpClient which is setup for the test host
+            var client = host.GetTestClient();
+
+            // Act
+            var response = await client.GetAsync("/Home/Test");
+
+            // Assert
+            var responseString = await response.Content.ReadAsStringAsync();
+            Assert.Equal("", responseString);
+        }
+    }
+}
---
+++ backend/Tests/VeryDeli.Tests.Integration/TestInfrastructure/Startup/TestServerStartup.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VeryDeli.Tests.Integration.TestInfrastructure.Startup
+{
+    public abstract class TestServerStartup
+    {
+    }
+}
