e45324f16ef8c91c33e0de22f3c6e459d3b7d38e
Kasper Cisewski <kaspercisewski@gmail.com>
2021-07-27 22:42:10 +0200

    https://trello.com/c/EhQHT7WA || added some basic infrastructure for order module

---
+++ backend/Modules/Order/VeryDeli.Modules.Order.Api/Controllers/OrderController.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace VeryDeli.Modules.Order.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public OrderController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] OrderModel orderModel)
+        {
+            try
+            {
+                var user = HttpContext.GetUser();
+                return Ok(await _mediator.Execute(new CreateOrderCommand() { OrderModel = orderModel, UserId = user.Id }));
+            }
+            catch (Exception e)
+            {
+                return BadRequest();
+            }
+        }
+
+    }
+}
---
+++ backend/Modules/Order/VeryDeli.Modules.Order.Logic/Commands/CreateOrderCommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VeryDeli.Modules.Order.Logic.Commands
+{
+    class CreateOrderCommand : IRequest
+    {
+    }
+}
